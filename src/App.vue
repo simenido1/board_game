a84701dcab34657f751eb79fa161a791bab27ac6
simenido <simenido@csgo.com>
2024-09-16 23:22:12 +0300

    feature: image draw on canvas

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,77 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted } from "vue";
+let canvas: HTMLCanvasElement;
+let ctx: CanvasRenderingContext2D;
+
+const numColumns = 20;
+const numRows = 20;
+
+function drawHorizontalLine(startX: number, endX: number, y: number) {
+  ctx.beginPath();
+  ctx.moveTo(startX, y);
+  ctx.lineTo(endX, y);
+  ctx.stroke();
+}
+
+function drawVerticalLine(startY: number, endY: number, x: number) {
+  ctx.beginPath();
+  ctx.moveTo(x, startY);
+  ctx.lineTo(x, endY);
+  ctx.stroke();
+}
+
+function drawImageScaled(img: HTMLImageElement, ctx: CanvasRenderingContext2D) {
+  const canvas = ctx.canvas;
+  const hRatio = canvas.width / img.width;
+  const vRatio = canvas.height / img.height;
+  const ratio = Math.min(hRatio, vRatio);
+  const centerShift_x = (canvas.width - img.width * ratio) / 2;
+  const centerShift_y = (canvas.height - img.height * ratio) / 2;
+  const dw = img.width*ratio;
+  const dh = img.height*ratio;
+  ctx.clearRect(0,0,canvas.width, canvas.height);
+  ctx.drawImage(img, 0,0, img.width, img.height,
+      centerShift_x,centerShift_y,dw, dh);
+
+  return {startX: centerShift_x, startY: centerShift_y, endX: dw + centerShift_x, endY: dh + centerShift_y};
+}
+
+onMounted(() => {
+  canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+  // const drawImageOnCanvas = (e: Event) => {
+  //   ctx.drawImage(e.target as HTMLImageElement, 0, 0);
+  // }
+  canvas.width = 0.98*window.innerWidth;
+  canvas.height = 0.98*window.innerHeight;
+})
+
+const drawImage = () => {
+  const image = document.getElementById("mainImage") as HTMLImageElement;
+  const imageCoords = drawImageScaled(image, ctx);
+  console.log(imageCoords);
+
+  for (let i = 1; i < numRows; i++) {
+    const yCoord = i * ctx.canvas.height / numRows;
+    drawHorizontalLine(imageCoords.startX, imageCoords.endX, yCoord);
+  }
+
+  for (let j = 1; j < numColumns; j++) {
+    const xCoord = imageCoords.startX + j * (imageCoords.endX - imageCoords.startX) / numColumns;
+    drawVerticalLine(imageCoords.startY, imageCoords.endY, xCoord);
+  }
+}
+
+
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<div>
+  <canvas id="canvas"></canvas>
+  <img src="/painted.png" alt="" style="display: none" id="mainImage" @load="drawImage" />
+</div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
